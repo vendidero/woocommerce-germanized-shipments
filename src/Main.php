cc25327bfbc1bf7c952c0f0c1f74cb3753176367
Dennis Nissle <info@vendidero.de>
2019-08-05 15:47:06 +0200

    added package.

---
+++ src/Main.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Initializes blocks in WordPress.
+ *
+ * @package WooCommerce/Blocks
+ */
+namespace Vendidero\Germanized\DHL;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Library class.
+ */
+class Main {
+
+    /**
+     * Initialize block library features.
+     */
+    public static function init() {
+
+    }
+}
---
+++ src/Package.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Returns information about the package and handles init.
+ *
+ * @package Automattic/WooCommerce/RestApi
+ */
+namespace Vendidero\Germanized\DHL;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main package class.
+ */
+class Package {
+    /**
+     * Version.
+     *
+     * @var string
+     */
+    const VERSION = '1.1.0-dev';
+    /**
+     * Init the package - load the REST API Server class.
+     */
+    public static function init() {
+        Main::init();
+    }
+    /**
+     * Return the version of the package.
+     *
+     * @return string
+     */
+    public static function get_version() {
+        return self::VERSION;
+    }
+    /**
+     * Return the path to the package.
+     *
+     * @return string
+     */
+    public static function get_path() {
+        return dirname( __DIR__ );
+    }
+}
