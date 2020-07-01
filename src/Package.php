4438e7abe3776c5f5860f6a61f1f5f4d2bc100d9
Dennis Nissle <info@vendidero.de>
2020-07-01 10:21:03 +0200

    Version bump.
    Signed-off-by: Dennis Nissle <info@vendidero.de>

--- src/Package.php
+++ src/Package.php
@@ -17,7 +17,7 @@ class Package {
      *
      * @var string
      */
-    const VERSION = '1.2.8';
+    const VERSION = '1.2.9';
 
     public static $upload_dir_suffix = '';
 
--- woocommerce-germanized-shipments.php
+++ woocommerce-germanized-shipments.php
@@ -5,7 +5,7 @@
  * Description: The Germanized Shipments integration, installed as a feature plugin for development and testing purposes.
  * Author: vendidero
  * Author URI: https://vendidero.de
- * Version: 1.2.8
+ * Version: 1.2.9
  * Requires PHP: 5.6
  * License: GPLv3
  *
