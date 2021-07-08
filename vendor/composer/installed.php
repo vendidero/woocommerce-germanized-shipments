d7f8e5d7e771573fe9484a0ead224b429e048b30
Dennis Nissle <info@vendidero.de>
2021-07-08 14:11:24 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dd8d5a16286c37e0d527ca13f300430087208af5',
+    'name' => 'vendidero/woocommerce-germanized-shipments',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.2',
+      'version' => '2.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '79a541381c1617b02980ab48d4ed0a0fb6ba462d',
+    ),
+    'dvdoug/boxpacker' => 
+    array (
+      'pretty_version' => '3.9.1',
+      'version' => '3.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '539084bd7b55056fa8b8b789e8ad5d3ebf5e5c3e',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'setasign/fpdf' => 
+    array (
+      'pretty_version' => '1.8.3',
+      'version' => '1.8.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a83253ece0df1c5b6c05fe7a900c660ae38afc3',
+    ),
+    'setasign/fpdi' => 
+    array (
+      'pretty_version' => 'v2.3.6',
+      'version' => '2.3.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6231e315f73e4f62d72b73f3d6d78ff0eed93c31',
+    ),
+    'vendidero/woocommerce-germanized-shipments' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd8d5a16286c37e0d527ca13f300430087208af5',
+    ),
+  ),
+);
