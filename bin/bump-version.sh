f2a2e558662c3b6e74bf7a1f09277515f0e27d10
vendidero <info@vendidero.de>
2022-07-20 15:19:03 +0200

    Version bump improvements.

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # Allow passing a custom version string - defaults to "next"
-VERSION=${1:-next}
+SMOOTH_BUMP=false
+VERSION=''
+
+print_usage() {
+  printf "./bump-version.sh -s [version_string]"
+}
+
+while getopts 'sv:' flag; do
+  case "${flag}" in
+    s) SMOOTH_BUMP=true ;;
+    v) VERSION="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
 
 LAST_PACKAGE_JSON=$(perl -ne 'print $1 while /\s*"version":\s\"(\d+\.\d+\.\d+)\"/sg' package.json)
 LAST_PACKAGE=$(perl -ne 'print $1 while /\s*const\sVERSION\s=\s'\''(\d+\.\d+\.\d+)'\''/sg' src/Package.php)
@@ -14,8 +28,14 @@ echo "Latest version detected in files: $LATEST"
 NEXT_VERSION=$(echo ${LATEST} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
 # Set the version to next version in case no version has been passed
-if [ "$VERSION" == "next" ]; then
+if [ "$VERSION" == "" ]; then
     VERSION=$NEXT_VERSION
+else
+    TMP_LAST=$(printf "$LATEST\n$VERSION" | sort -V -r | head -1)
+    # Do not bump the version in smooth mode in case the version has already been bumped.
+    if [ "$SMOOTH_BUMP" == "true" ] && [ "$TMP_LAST" == "$LATEST" ]; then
+        NEXT_VERSION=$LATEST
+    fi
 fi
 
 # Use the latest version: Either detected in files or from custom argument
