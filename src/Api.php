ac1bb201fbeba03d4d0266adf5f5c24e69711a3a
vendidero <info@vendidero.de>
2022-09-14 13:13:08 +0200

    Add hs_code + manufacture_country to products REST API.

--- src/Api.php
+++ src/Api.php
@@ -19,7 +19,56 @@ class Api {
 
 		add_filter( 'woocommerce_rest_shop_order_schema', array( __CLASS__, 'order_shipments_schema' ), 10 );
 		add_filter( 'woocommerce_rest_prepare_shop_order_object', array( __CLASS__, 'prepare_order_shipments' ), 10, 3 );
-		add_filter( 'woocommerce_rest_shop_order_schema', array( __CLASS__, 'schema' ) );
+		add_filter( 'woocommerce_rest_shop_order_schema', array( __CLASS__, 'order_schema' ) );
+
+		add_filter( 'woocommerce_rest_product_schema', array( __CLASS__, 'product_schema' ) );
+		add_filter( 'woocommerce_rest_product_variation_schema', array( __CLASS__, 'product_variation_schema' ) );
+
+		add_filter( 'woocommerce_rest_pre_insert_product_object', array( __CLASS__, 'update_product' ), 10, 2 );
+
+		add_filter( 'woocommerce_rest_prepare_product_object', array( __CLASS__, 'prepare_product' ), 10, 3 );
+		add_filter( 'woocommerce_rest_prepare_product_variation_object', array( __CLASS__, 'prepare_product' ), 10, 3 );
+	}
+
+	public static function prepare_product( $response, $object, $request ) {
+		if ( $product = wc_get_product( $object ) ) {
+			$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+
+			$response->set_data( array_merge( $response->data, self::get_product_data( $product, $context ) ) );
+		}
+
+		return $response;
+	}
+
+	private static function get_product_data( $product, $context = 'view' ) {
+		$data = array();
+
+		if ( $shipments_product = wc_gzd_shipments_get_product( $product ) ) {
+			$data['hs_code']             = $shipments_product->get_hs_code( $context );
+			$data['manufacture_country'] = $shipments_product->get_manufacture_country( $context );
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param \WC_Product $product
+	 * @param $request
+	 *
+	 * @return \WC_Product $product
+	 */
+	public static function update_product( $product, $request ) {
+		if ( $shipments_product = wc_gzd_shipments_get_product( $product ) ) {
+			if ( isset( $request['hs_code'] ) ) {
+				$shipments_product->set_hs_code( wc_clean( wp_unslash( $request['hs_code'] ) ) );
+			}
+
+			if ( isset( $request['manufacture_country'] ) ) {
+				$shipments_product->set_manufacture_country( wc_clean( wp_unslash( $request['manufacture_country'] ) ) );
+			}
+		}
+
+		return $product;
 	}
 
 	public static function remove_status_prefix( $status ) {
@@ -34,13 +83,12 @@ class Api {
 	 * Extend schema.
 	 *
 	 * @since 1.0.0
-	 * @wp-hook woocommerce_rest_customer_schema
 	 *
-	 * @param array $schema_properties Data used to create the customer.
+	 * @param array $schema_properties Data used to create the order.
 	 *
 	 * @return array
 	 */
-	public static function schema( $schema_properties ) {
+	public static function order_schema( $schema_properties ) {
 		$statuses = array_map( array( __CLASS__, 'remove_status_prefix' ), array_keys( wc_gzd_get_shipment_order_shipping_statuses() ) );
 
 		$schema_properties['shipping_status'] = array(
@@ -49,6 +97,58 @@ class Api {
 			'enum'        => $statuses,
 			'context'     => array( 'view', 'edit' ),
 			'readonly'    => true,
+		);
+
+		return $schema_properties;
+	}
+
+	/**
+	 * Extend product variation schema.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $schema_properties Data used to create the product.
+	 *
+	 * @return array
+	 */
+	public static function product_variation_schema( $schema_properties ) {
+		$schema_properties['hs_code'] = array(
+			'description' => _x( 'HS-Code (Customs)', 'shipments', 'woocommerce-germanized-shipments' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' ),
+			'readonly'    => true,
+		);
+
+		$schema_properties['manufacture_country'] = array(
+			'description' => _x( 'Country of manufacture (Customs)', 'shipments', 'woocommerce-germanized-shipments' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' ),
+			'readonly'    => true,
+		);
+
+		return $schema_properties;
+	}
+
+	/**
+	 * Extend product schema.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $schema_properties Data used to create the product.
+	 *
+	 * @return array
+	 */
+	public static function product_schema( $schema_properties ) {
+		$schema_properties['hs_code'] = array(
+			'description' => _x( 'HS-Code (Customs)', 'shipments', 'woocommerce-germanized-shipments' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' ),
+		);
+
+		$schema_properties['manufacture_country'] = array(
+			'description' => _x( 'Country of manufacture (Customs)', 'shipments', 'woocommerce-germanized-shipments' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' ),
 		);
 
 		return $schema_properties;
