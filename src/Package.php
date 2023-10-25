e16521cb9be3e04966496810383bf39b9b09e6c1
vendidero <info@vendidero.de>
2023-10-25 16:35:58 +0200

    Version bump to 2.4.4.

--- src/Package.php
+++ src/Package.php
@@ -19,7 +19,7 @@ class Package {
 	 *
 	 * @var string
 	 */
-	const VERSION = '2.4.3';
+	const VERSION = '2.4.4';
 
 	public static $upload_dir_suffix = '';
 
--- woocommerce-germanized-shipments.php
+++ woocommerce-germanized-shipments.php
@@ -5,7 +5,7 @@
  * Description: The Germanized Shipments integration, installed as a feature plugin for development and testing purposes.
  * Author: vendidero
  * Author URI: https://vendidero.de
- * Version: 2.4.3
+ * Version: 2.4.4
  * Requires PHP: 5.6
  * License: GPLv3
  *
