e3720cfccf9578ca387a700f328a0c4967fd3ce4
vendidero <info@vendidero.de>
2024-11-25 12:21:42 +0100

    Refactor the label product change trigger to a separate js file which can be loaded for table and edit-order page requests.

--- assets/js/static/admin-shipments.js
+++ assets/js/static/admin-shipments.js
@@ -32,8 +32,7 @@ window.shipments.admin = window.shipments.admin || {};
                 .on( 'click', '#panel-order-shipments #order-shipment-add', self.onAddShipment )
                 .on( 'click', '#panel-order-shipments .remove-shipment', self.onRemoveShipment )
                 .on( 'click', '#panel-order-shipments button#order-shipments-save', self.onSave )
-                .on( 'click', '#panel-order-shipments .notice-dismiss', self.onRemoveNotice )
-                .on( 'change', '.shipments-create-label #product_id', self.onChangeLabelProductId );
+                .on( 'click', '#panel-order-shipments .notice-dismiss', self.onRemoveNotice );
 
             $( document )
                 .on( 'wc_gzd_shipments_admin_shipment_modal_after_submit_success', 'a#order-return-shipment-add', self.onAddReturnSuccess );
@@ -330,93 +329,6 @@ window.shipments.admin = window.shipments.admin || {};
             var self = shipments.admin.shipments;
 
             self.$wrapper.find( '.notice-wrapper' ).append( '<div class="notice is-dismissible notice-' + noticeType +'"><p>' + message + '</p><button type="button" class="notice-dismiss"></button></div>' );
-        },
-
-        onChangeLabelProductId: function() {
-            var self = shipments.admin.shipments;
-
-            self.showOrHideByLabelProduct( $( this ).val() );
-        },
-
-        showOrHideByLabelProduct: function( productId ) {
-            var $wrapper  = $( '.shipments-create-label' ),
-                $fields   = $wrapper.find( 'p.form-field :input[data-products-supported]' );
-
-            $fields.each( function() {
-                var $field    = $( this ),
-                    supported = $field.data( 'products-supported' );
-
-                if ( supported.length > 0 ) {
-                    if ( supported.indexOf( '&' ) > -1 && $field.is( 'select' ) ) {
-                        var supportedData = supported.split( '&' ).filter( Boolean ),
-                            needsReset = false;
-
-                        $.each( supportedData, function( i, d ) {
-                            var innerData = d.split( '=' ).filter( Boolean );
-
-                            if ( innerData.length > 1 ) {
-                                var optionKey = innerData[0];
-                                var supportedProducts = innerData[1].split( ',' );
-                                var isHidden = true;
-                                var $option = $field.find( 'option[value="' + optionKey + '"]' );
-
-                                if ( $option.length > 0 ) {
-                                    if ( $.inArray( productId, supportedProducts ) !== -1 ) {
-                                        isHidden = false;
-                                    }
-
-                                    if ( isHidden ) {
-                                        if ( $option.is( ':selected' ) ) {
-                                            needsReset = true;
-                                        }
-
-                                        $option.hide();
-                                    } else {
-                                        $option.show();
-                                    }
-                                }
-                            }
-                        } );
-
-                        var isFieldHidden = true;
-
-                        $field.find( 'option' ).each( function () {
-                            if ( $( this ).css( 'display' ) !== 'none' ) {
-                                if ( needsReset ) {
-                                    $( this ).prop("selected", true );
-                                }
-                                isFieldHidden = false;
-                                return false;
-                            }
-                        });
-
-                        if ( isFieldHidden ) {
-                            $field.parents( '.form-field' ).hide();
-                            $field.trigger( 'change' );
-                        } else {
-                            $field.parents( '.form-field' ).show();
-                            $field.trigger( 'change' );
-                        }
-
-                        $field.trigger( 'change' );
-                    } else {
-                        var supportedProducts = supported.split( ',' ).filter( Boolean ),
-                            isHidden = supportedProducts.length > 0 ? true : false;
-
-                        if ( $.inArray( productId, supportedProducts ) !== -1 ) {
-                            isHidden = false;
-                        }
-
-                        if ( isHidden ) {
-                            $field.parents( '.form-field' ).hide();
-                            $field.trigger( 'change' );
-                        } else {
-                            $field.parents( '.form-field' ).show();
-                            $field.trigger( 'change' );
-                        }
-                    }
-                }
-            } );
         },
 
         getParams: function() {
